dfbb94992db54bdd09b3bc000f95ee4384102139
jadick <adickja@gmail.com>
2021-08-21 21:34:13 -0400

    api linked

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import "./App.css";
+import { Link, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { baseURL, config } from "./services";
 
 function App() {
+
+  const [equipment, setEquipment] = useState([])
+
+  useEffect(() => {
+    const getEquipment = async () => {
+      const resp = await axios.get(baseURL, config)
+      setEquipment(resp.data)
+    }
+    getEquipment()
+  }, [])
+
+  console.log(equipment)
+
   return (
     <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      
     </div>
-  );
+  )
 }
 
 export default App;
