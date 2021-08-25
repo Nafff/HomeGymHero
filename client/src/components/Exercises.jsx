f1d22f1792a3399bc4c20d1e781cdfc1778c6e63
jadick <adickja@gmail.com>
2021-08-25 09:53:41 -0400

    morning commit

--- client/src/components/Exercises.jsx
+++ client/src/components/Exercises.jsx
@@ -1,26 +1,33 @@
 import { useState, useEffect } from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Exercises(props) {
-
-  const availEx = props.exercises.filter(exercise => props.equipment.includes(exercise.fields.equipment))
+  const availEx = props.exercises.filter((exercise) =>
+    props.equipment.includes(exercise.fields.equipment)
+  );
 
   const handleAdd = (event) => {
-    event.preventDefault()
-    props.setWorkout(props.workout.concat(event.target.value))
-    console.log(event.target.value)
-  }
+    event.preventDefault();
+    props.setWorkout(props.workout.concat(event.target.value));
+    console.log(event.target.value);
+  };
 
   return (
     <>
-      <h1>Hello here are exercises</h1>
       {availEx.map((exercise) => (
         <>
-          <Link to={`/exerciseinfo/${exercise.id}`}>{exercise.fields.exercise}</Link> <button onClick={handleAdd} value={exercise.fields.exercise}>Add To Workout</button>
+          <Link to={`/exerciseinfo/${exercise.id}`}>
+            {exercise.fields.exercise}
+          </Link>{" "}
+          <button onClick={handleAdd} value={exercise.fields.exercise}>
+            Add To Workout
+          </button>
+          <br />
         </>
-      ))
-      }
-      <Link to={"/new/"}><button>Create New Exercise</button></Link>
+      ))}
+      <Link to={"/new/"}>
+        <button>Create New Exercise</button>
+      </Link>
     </>
   );
 }
